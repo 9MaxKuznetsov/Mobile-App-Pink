9d28ad77958a141a8c23cbc9137fdace1a6531b9
AlexanderBrez <jztenk@gmail.com>
2018-05-16 23:29:51 +0300

    Fixed pop-ups & :focus

--- source/js/popups.js
+++ source/js/popups.js
@@ -2,11 +2,13 @@ var open = document.querySelector(".contest__submit");
 var failure = document.querySelector(".pop-up--failure");
 var success = document.querySelector(".pop-up--success");
 var form = document.querySelector("form");
-var text = document.querySelector(".desribe__field");
+var fname = document.querySelector("[name=first-name]");
+var lname = document.querySelector("[name=last-name]");
+var email = document.querySelector("[name=e-mail]");
 var sclose = document.querySelector(".pop-up__button--success");
 var eclose = document.querySelector(".pop-up__button--failure");
 form.addEventListener("submit", function (evt) {
-  if (!text.value) {
+  if (!fname.value || !lname.value || !email.value ) {
   evt.preventDefault();
   failure.classList.add("pop-up-show");
 } else {
