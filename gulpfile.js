b74104dc8d8206d330c551d667dd6962480fc481
AlexanderBrez <jztenk@gmail.com>
2018-05-16 22:02:23 +0300

    gulp build

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,13 @@ var less = require("gulp-less");
 var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
+var minify = require("gulp-csso");
+var rename = require("gulp-rename");
+var imagemin = require("gulp-imagemin");
 var server = require("browser-sync").create();
+var webp = require("gulp-webp");
+var run = require("run-sequence");
+var svgstore = require("gulp-svgstore");
 
 gulp.task("style", function() {
   gulp.src("source/less/style.less")
@@ -15,7 +21,9 @@ gulp.task("style", function() {
       autoprefixer()
     ]))
     .pipe(gulp.dest("source/css"))
-    .pipe(server.stream());
+    .pipe(minify())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("source/css"));
 });
 
 gulp.task("serve", ["style"], function() {
@@ -29,4 +37,46 @@ gulp.task("serve", ["style"], function() {
 
   gulp.watch("source/less/**/*.less", ["style"]);
   gulp.watch("source/*.html").on("change", server.reload);
+});
+
+gulp.task("copy", function () {
+  return gulp.src([
+    "source/fonts/**/*.{woff,worff2}",
+    "source/img/**",
+    "source/js/**",
+    "source/css/**",
+    "source/*.html"
+  ], {
+    base: "source"
+  })
+  .pipe(gulp.dest("build"));
+});
+
+gulp.task("images", function() {
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationlevel: 3}),
+      imagemin.jpegtran({progressive: true}),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("source/img"));
+});
+
+gulp.task("webp", function() {
+  return gulp.src("source/img/**/*.{png,jpg}")
+    .pipe(webp({quality: 90}))
+    .pipe(gulp.dest("source/img"));
+});
+
+gulp.task("sprite", function () {
+  return gulp.src("source/img/icon-*.svg")
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("source/img"));
+});
+
+gulp.task("build", function (done) {
+  run("style", "sprite", "images", "copy", done);
 });
