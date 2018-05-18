856e270a298b6c439cd5827094a2f530f57ad531
AlexanderBrez <jztenk@gmail.com>
2018-05-18 17:08:10 +0300

    some fixes

--- source/js/popups.js
+++ source/js/popups.js
@@ -1,6 +1,7 @@
 var open = document.querySelector(".contest__submit");
 var failure = document.querySelector(".pop-up--failure");
 var success = document.querySelector(".pop-up--success");
+var center = document.querySelector(".pop-up-center");
 var form = document.querySelector("form");
 var fname = document.querySelector("[name=first-name]");
 var lname = document.querySelector("[name=last-name]");
@@ -10,10 +11,18 @@ var eclose = document.querySelector(".pop-up__button--failure");
 form.addEventListener("submit", function (evt) {
   if (!fname.value || !lname.value || !email.value ) {
   evt.preventDefault();
+  email.classList.add("form__text-input--required");
+  lname.classList.add("form__text-input--required");
+  fname.classList.add("form__text-input--required");
   failure.classList.add("pop-up-show");
+  lname.classList.add("error");
+  email.classList.add("error");
 } else {
       evt.preventDefault();
       success.classList.add("pop-up-show");
+      email.classList.remove("form__text-input--required");
+      lname.classList.remove("form__text-input--required");
+      fname.classList.remove("form__text-input--required");
 }
 });
 if (eclose) {
