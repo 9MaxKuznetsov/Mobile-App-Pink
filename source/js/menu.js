e53d7d5212bd3a6a5cabd5fb7a8fa02522b895e4
AlexanderBrez <jztenk@gmail.com>
2018-05-22 19:15:41 +0300

    js menu update

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,45 +1,20 @@
 var burger = document.querySelector(".site-list-actions__close");
-var burgerindex = document.querySelector(".site-list-actions__menu--index");
 var header = document.querySelector(".page-header");
-var handh = document.querySelector(".page-header--hand");
 var sitelist = document.querySelector(".site-list");
-var actions = document.querySelector(".site-list-actions");
 var wrapper = document.querySelector(".page-header-wrapper");
 var menu = document.querySelector(".site-list-actions__menu");
-var headerdefault = document.querySelector(".page-header--default");
-var phhand = document.querySelector(".page-header__hand");
-
-header.classList.remove("page-header--show");
-if (handh) {
-  handh.classList.remove("page-header--hand-show");
-  phhand.classList.remove("page-header__hand--show");
-}
-sitelist.classList.remove("site-list--show");
-wrapper.classList.remove("page-header-wrapper--show");
-burger.classList.remove("site-list-actions__close--show");
-actions.classList.remove("site-list-actions--show");
-menu.classList.add("site-list-actions__menu--open");
 
 
-menu.addEventListener('click', function() {
+var menutoggle = function() {
+  sitelist.classList.toggle("site-list--show");
+  header.classList.toggle("page-header--show");
+  burger.classList.toggle("site-list-actions__close--show");
+  wrapper.classList.toggle("page-header-wrapper--show");
   menu.classList.toggle("site-list-actions__menu--open");
-})
+};
 
-if (phhand) {
-  burger.addEventListener("click", function (evt) {
-      evt.preventDefault();
-      phhand.classList.toggle("page-header__hand--show");
-      handh.classList.toggle("page-header--hand-show");
+menutoggle();
+burger.addEventListener('click', function () {
+  menutoggle();
+  menu.classList.toggle("site-list-actions__menu--no-cross");
 });
-};
-
-if (burger) {
-  burger.addEventListener("click", function (evt) {
-      evt.preventDefault();
-      sitelist.classList.toggle("site-list--show");
-      header.classList.toggle("page-header--show");
-      actions.classList.toggle("site-list-actions--show");
-      burger.classList.toggle("site-list-actions__close--show");
-      wrapper.classList.toggle("page-header-wrapper--show");
-    });
-};
