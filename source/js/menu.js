43877605139b8862eeaf70fbb97d0d102ef405db
AlexanderBrez <jztenk@gmail.com>
2018-05-24 01:38:45 +0300

    B11 B14 B16 B17

--- source/js/menu.js
+++ source/js/menu.js
@@ -3,7 +3,8 @@ var header = document.querySelector(".page-header");
 var sitelist = document.querySelector(".site-list");
 var wrapper = document.querySelector(".page-header-wrapper");
 var menu = document.querySelector(".site-list-actions__menu");
-
+var def = document.querySelector(".page-header--default");
+var hand = document.querySelector(".page-header--hand");
 
 var menutoggle = function() {
   sitelist.classList.toggle("site-list--show");
@@ -17,4 +18,11 @@ menutoggle();
 burger.addEventListener('click', function () {
   menutoggle();
   menu.classList.toggle("site-list-actions__menu--no-cross");
+  if (def) {
+    def.classList.toggle("page-header--show-default");
+  };
+
+  if (hand) {
+    hand.classList.toggle("page-header--show-hand");
+  };
 });
